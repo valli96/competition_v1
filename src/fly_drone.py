6f338a4a762b769f937f4e0d3f68e16ac431d4e8
valli96 <valentin.resapow@tuhh.de>
2022-01-12 17:21:29 +0200

    drone imitates the angel marker !!! working !!!

--- src/fly_drone.py
+++ src/fly_drone.py
@@ -46,17 +46,16 @@ pub_land = rospy.Publisher("/bebop/land", Empty, queue_size=1)
 
 diff_ang = 0
 first_run = True
-initial_odom = [0, 0, 0, 0]  # [x , y , z, yaw]
-initial_odom_theta = 0
+initial_odom = [0, 0, 0]  # [x , y , z, yaw]
+initial_angel = 0
 
-
+yaw_marker = 0
 
 speed_factor = 1
 custom_height = 1
 custom_x = 0.0
 custom_y = 0.0
-custom_angel = 50 * np.pi/180
-
+custom_angel = 50*np.pi/180
 
 def callback(msg):
     '''
@@ -66,6 +65,7 @@ def callback(msg):
     '''
     global first_run
     global initial_odom
+    global initial_angel
     global custom_x
     global custom_y
     global custom_height
@@ -77,14 +77,21 @@ def callback(msg):
                 msg.pose.pose.position.z]
 
     rot_q = msg.pose.pose.orientation
-    (roll, pitch, yaw) = euler_from_quaternion(
+    (roll, pitch, yaw_drone) = euler_from_quaternion(
         [rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-    ################## odom message of drone ############################### 
-    
+
+    if yaw_drone > 0:
+        yaw_drone = yaw_drone - math.pi
+    elif yaw_drone < 0:
+        yaw_drone = yaw_drone + math.pi
+
+    ################## odom message of drone ################################
+
     ################################### calibration IMU ######################
     if first_run == True:
 
-        initial_odom = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z, yaw]
+        initial_odom = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+        initial_angel = yaw_drone
         first_run = False
     ################################### calibration IMU ######################
 
@@ -92,18 +99,18 @@ def callback(msg):
     pos_done[0] = (msg.pose.pose.position.x - initial_odom[0]) - custom_x # custom_x
     pos_done[1] = (msg.pose.pose.position.y - initial_odom[1]) - custom_y
     pos_done[2] = (msg.pose.pose.position.z - initial_odom[2]) - custom_height  # - 2 the number will define how heigh the drone can hover 
-    drone_rot = (msg.pose.pose.position.z - initial_odom[3]) - custom_angel  #  this is the yaw of the drone
+    # drone_rot = (msg.pose.pose.position.z - initial_odom[3]) - initial_angel  #  this is the yaw of the drone
     
     # print(str(msg.pose.pose.position.z) + ": odom rotion ")
     # print(str(initial_odom[3]) + ":  initial rotaion")
     # print(str(custom_angel) + "custom angel")
 
-    print(str(drone_rot) + ":   drone_rot")
+    print(str(yaw_drone) + ":   drone_rot")
     
     pid_x = PID(0.3, 0.05, 0.01, setpoint=0)
     # pid_y = PID(0.3, 0.05, 0.01, setpoint=0)
     pid_z = PID(0.5, 0.01, 0.05, setpoint=0)
-    pid_rot = PID(0.5, 0.01, 0.05, setpoint=0)
+    pid_rot = PID(0.5, 0.02, 0.00, setpoint=0)
 
     # speed.linear.x = pid_x(pos_done[0])
     # speed.linear.y = 0
@@ -112,13 +119,18 @@ def callback(msg):
     speed.linear.z = 0
     speed.linear.x = 0
     speed.linear.y = 0
-    speed.angular.z =  pid_rot(drone_rot/10)
+
+
+    speed.angular.z =  -pid_rot(yaw_marker)/2
     pub_move.publish(speed)
     ##################### adjust to target pos and pub speed ######################
-    print(str(speed.angular.z) + ":   the angular velocity")
-    print(str(speed.linear.x)  + ":   the velocity in x")
-    print(str(speed.linear.y)  + ":   the velocity in y")
-    print(str(speed.linear.z)  + ":   the velocity in z")
+
+
+    with open('test_data.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([speed.angular.z    , yaw_drone, yaw_marker])
+
+
 def custom_command(msg):
     global custom_height
     custom_height = msg.data/100
@@ -126,6 +138,7 @@ def custom_command(msg):
     
 
 def look_for_marker():
+    pub_land.publish(Empty_)
     global state_of_operation
     if state_of_operation == 0 or state_of_operation == 2:
         state_of_operation = 1  
@@ -176,6 +189,7 @@ def get_maker_pos_2(msg):
         global timer_marker
         global state_of_operation
         global goal_pos
+        global yaw_marker
         state_of_operation = 1
 
         ######################### position ################################
@@ -183,24 +197,27 @@ def get_maker_pos_2(msg):
                                 msg.markers[0].pose.pose.position.y,
                                 msg.markers[0].pose.pose.position.z]
 
-
         goal_pos.x = vec_pos_mark_m_space[0] #- math.cos(diff_ang)*0.5
         goal_pos.y = -vec_pos_mark_m_space[1] #- math.sin(diff_ang)*0.5
         goal_pos.z = vec_pos_mark_m_space[2]
         print(vec_pos_mark_m_space)
         ######################### position ################################
 
+        print(str(goal_pos.x)+ ":    x")
+        print(str(goal_pos.y)+ ":    y")
+        print(str(goal_pos.z)+ ":    z")
+
         ######################### rotation ################################
-        (pitch, roll, yaw) = euler_from_quaternion(
+        (pitch, roll, yaw_marker) = euler_from_quaternion(
             [msg.markers[0].pose.pose.orientation.x,
              msg.markers[0].pose.pose.orientation.y, 
              msg.markers[0].pose.pose.orientation.z, 
              msg.markers[0].pose.pose.orientation.w])
         # rotation offest fixed. before 90 now 0 
-        if yaw > -math.pi/2:
-            diff_ang = yaw - math.pi/2
-        elif yaw < -math.pi/2:
-            diff_ang = yaw + 3*math.pi/2
+        if yaw_marker > -math.pi/2:
+            yaw_marker = yaw_marker - math.pi/2
+        elif yaw_marker < -math.pi/2:
+            yaw_marker = yaw_marker + 3*math.pi/2
         ######################### rotation ################################
         
         ################ writing to csv File ##############################
@@ -208,18 +225,24 @@ def get_maker_pos_2(msg):
         #         writer = csv.writer(f)
         #         writer.writerow([goal_pos.x, goal_pos.y,goal_pos.z])
             
-        with open('test_data.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow([diff_ang, diff_ang, yaw])
+        # with open('test_data.csv', 'a') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow([yaw_marker, yaw_marker, yaw_marker])
         ############### writing to csv File ###############################
 
         ################ for the marker looking routine ###################
         timer_marker.cancel()
-        timer_marker = threading.Timer(2, look_for_marker)
+        timer_marker = threading.Timer(0.5, look_for_marker)
         timer_marker.start()    
         ################ for the marker looking routine ###################
-
-###################### if the process is killed ##########################################
+    else:
+        speed.linear.z = 0
+        speed.linear.x = 0
+        speed.linear.y = 0
+        speed.angular.z = 0
+        pub_move.publish(speed)
+        time.sleep(3)
+        ###################### if the process is killed ##########################################
 def myhook():
     pub_land.publish(Empty_)
     print("shutdown time!")
@@ -233,15 +256,16 @@ def myhook():
 rospy.on_shutdown(myhook)
 ##################### if the process is killed ##########################################
 
+# drone imitates the angel marker !!! working !!!
 
 def main():
     # global state_of_operation
     global timer_marker
     time.sleep(0.5)
-    # pub_takeoff.publish(Empty_)
+    pub_takeoff.publish(Empty_)
     time.sleep(2.5)
     rospy.Subscriber("/ar_pose_marker", AlvarMarkers, get_maker_pos_2, queue_size=1)  # get marker position
-    # rospy.Subscriber("/bebop/odom", Odometry, callback, queue_size=1)         # is used for debugging and controll the drone without the marker
+    rospy.Subscriber("/bebop/odom", Odometry, callback, queue_size=1)         # is used for debugging and controll the drone without the marker
     # rospy.Subscriber("/bebop/odom", Odometry, main_algorithm, queue_size=1)     # main function (state machine)
     # rospy.Subscriber("/custom_command", Float32, custom_command, queue_size=1)  # resice and handels commands from the UI
 
