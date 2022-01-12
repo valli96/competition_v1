rotation offest fixed. before 90 now 0

src/fly_drone.py
@@ -5,8 +5,8 @@ from geometry_msgs.msg import Pose, Twist, Point
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Empty, String, Float32, Header
 from visualization_msgs.msg import Marker
-from ar_track_alvar_msgs.msg import AlvarMarker
 from tf import TransformListener, Transformer
+from ar_track_alvar_msgs.msg  import AlvarMarkers
 # from sensor_msgs.msg import CompressedImage
 
 ## general imports
@@ -55,7 +55,7 @@ speed_factor = 1
 custom_height = 1
 custom_x = 0.0
 custom_y = 0.0
-custom_angel = 0.0
+custom_angel = 50 * np.pi/180
 
 
 def callback(msg):
@@ -94,19 +94,31 @@ def callback(msg):
     pos_done[2] = (msg.pose.pose.position.z - initial_odom[2]) - custom_height  # - 2 the number will define how heigh the drone can hover 
     drone_rot = (msg.pose.pose.position.z - initial_odom[3]) - custom_angel  #  this is the yaw of the drone
     
+    # print(str(msg.pose.pose.position.z) + ": odom rotion ")
+    # print(str(initial_odom[3]) + ":  initial rotaion")
+    # print(str(custom_angel) + "custom angel")
+
+    print(str(drone_rot) + ":   drone_rot")
+    
     pid_x = PID(0.3, 0.05, 0.01, setpoint=0)
     # pid_y = PID(0.3, 0.05, 0.01, setpoint=0)
     pid_z = PID(0.5, 0.01, 0.05, setpoint=0)
     pid_rot = PID(0.5, 0.01, 0.05, setpoint=0)
 
-    speed.linear.x = pid_x(pos_done[0])
-    speed.linear.y = 0
+    # speed.linear.x = pid_x(pos_done[0])
+    # speed.linear.y = 0
     # speed.linear.z = pid_z(pos_done[2])
+    # speed.linear.z = 0
     speed.linear.z = 0
-    speed.angular.z =  pid_rot(drone_rot)
-    # pub_move.publish(speed)
+    speed.linear.x = 0
+    speed.linear.y = 0
+    speed.angular.z =  pid_rot(drone_rot/10)
+    pub_move.publish(speed)
     ##################### adjust to target pos and pub speed ######################
-
+    print(str(speed.angular.z) + ":   the angular velocity")
+    print(str(speed.linear.x)  + ":   the velocity in x")
+    print(str(speed.linear.y)  + ":   the velocity in y")
+    print(str(speed.linear.z)  + ":   the velocity in z")
 def custom_command(msg):
     global custom_height
     custom_height = msg.data/100
@@ -179,24 +191,27 @@ def get_maker_pos_2(msg):
         ######################### position ################################
 
         ######################### rotation ################################
-        (roll, pitch, yaw) = euler_from_quaternion(
-            [msg.markers[0].pose.pose.orientation.x, msg.markers[0].pose.pose.orientation.y, msg.markers[0].pose.pose.orientation.z, msg.markers[0].pose.pose.orientation.w])
-        theta_marker = roll
-        if theta_marker >= 0:
-            diff_ang = -(theta_marker - math.pi)
-        if theta_marker < 0:
-            diff_ang = -(theta_marker + math.pi)
-        # print(round(theta_marker, 3), "theta angel")
+        (pitch, roll, yaw) = euler_from_quaternion(
+            [msg.markers[0].pose.pose.orientation.x,
+             msg.markers[0].pose.pose.orientation.y, 
+             msg.markers[0].pose.pose.orientation.z, 
+             msg.markers[0].pose.pose.orientation.w])
+
+        
+        if yaw > -math.pi/2:
+            diff_ang = yaw - math.pi/2
+        elif yaw < -math.pi/2:
+            diff_ang = yaw + 3*math.pi/2
         ######################### rotation ################################
         
         ################ writing to csv File ##############################
-        with open('test_data.csv', 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([goal_pos.x, goal_pos.y,goal_pos.z])
-            
         # with open('test_data.csv', 'a') as f:
-        #     writer = csv.writer(f)
-        #     writer.writerow([msg.pose.pose.position.z - initial_odom_z, speed.linear.z])
+        #         writer = csv.writer(f)
+        #         writer.writerow([goal_pos.x, goal_pos.y,goal_pos.z])
+            
+        with open('test_data.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([diff_ang, diff_ang, yaw])
         ############### writing to csv File ###############################
 
         ################ for the marker looking routine ###################
@@ -226,10 +241,10 @@ def main():
     time.sleep(0.5)
     # pub_takeoff.publish(Empty_)
     time.sleep(2.5)
-    rospy.Subscriber("/bebop/odom", Odometry, main_algorithm, queue_size=1)     # main function (state machine)
-    rospy.Subscriber("/ar_pose_marker", Marker, get_maker_pos_2, queue_size=1)  # get marker position
-    rospy.Subscriber("/custom_command", Float32, custom_command, queue_size=1)  # resice and handels commands from the UI
+    rospy.Subscriber("/ar_pose_marker", AlvarMarkers, get_maker_pos_2, queue_size=1)  # get marker position
     # rospy.Subscriber("/bebop/odom", Odometry, callback, queue_size=1)         # is used for debugging and controll the drone without the marker
+    # rospy.Subscriber("/bebop/odom", Odometry, main_algorithm, queue_size=1)     # main function (state machine)
+    # rospy.Subscriber("/custom_command", Float32, custom_command, queue_size=1)  # resice and handels commands from the UI
 
     
     timer_marker = threading.Timer(5,look_for_marker) # wait 5 seconds before starting to look for the marker 